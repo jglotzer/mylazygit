6c9cbb4311e7c15423520df6fca547674929f107
John Glotzer <jglotzer@gmail.com>
2025-01-07 12:31:15 -0600

    Move blocks in Visual Mode.

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,3 +2,7 @@
 vim.keymap.set("n", "<F9>", "<C-W><C-R>", { desc = "Switch Diff panes around", noremap = true, silent = true })
 vim.keymap.set("n", "<F12>", "<C-W><C-W>", { desc = "Go to other Window", noremap = true, silent = true })
 vim.keymap.set("n", "<F11>", "<Cmd>set nu! rnu! <CR>", { desc = "Toggle line numbers", noremap = true, silent = true })
+
+-- " In visual mode move selected blocks up and down
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", {})
+vim.keymap.set("v", "K", ":m '>-2<CR>gv=gv", {})
