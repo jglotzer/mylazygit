d460886b9cdca1bfb14d96464e33e4920937dbdb
John Glotzer <jglotzer@gmail.com>
2025-01-07 11:45:20 -0600

    Mappings for F9, F11, F12

--- init.lua
+++ init.lua
@@ -3,5 +3,6 @@ require("config.sets")
 -- Seems like some things have to be done as config and not as plugins.
 require("config.lualine")
 require("config.yankstack")
+require("config.keymaps")
 require("config.telescope.keymaps")
 require("config.nerdtree.keymaps")
---
+++ lua/config/keymaps.lua
@@ -0,0 +1,4 @@
+
+vim.keymap.set("n", "<F9>", "<C-W><C-R>", { desc = "Switch Diff panes around", noremap = true, silent = true })
+vim.keymap.set("n", "<F12>", "<C-W><C-W>", { desc = "Go to other Window", noremap = true, silent = true })
+vim.keymap.set("n", "<F11>", "<Cmd>set nu! rnu! <CR>", { desc = "Toggle line numbers", noremap = true, silent = true })
