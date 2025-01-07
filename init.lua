a14652c3264a33a1730bea0c80f18910394ff0cd
John Glotzer <jglotzer@gmail.com>
2025-01-06 21:34:12 -0600

    Get Lua mapping of telescope keymaps

--- init.lua
+++ init.lua
@@ -3,9 +3,4 @@ require("config.sets")
 -- Seems like some things have to be done as config and not as plugins.
 require("config.lualine")
 require("config.yankstack")
-require("vim.highlight").on_yank({ timeout = 100 })
-
--- Some things have to be done later.
-local telescope_builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>tf', telescope_builtin.find_files, {})
-vim.keymap.set('n', '<leader>ta', telescope_builtin.live_grep, {})
+require("config.telescope.keymaps")
---
+++ lua/config/telescope/keymaps.lua
@@ -0,0 +1,29 @@
+
+local tbi = require('telescope.builtin')
+
+vim.keymap.set("n", "<leader>ta", tbi.grep_string, { desc = "search current word" })
+vim.keymap.set("n", "<leader>tg", tbi.live_grep, { desc = "live grep" })
+vim.keymap.set("n", "<leader>tf", tbi.find_files, { desc = "find files" })
+--" Or :Telescope grep_string search=ur_mom
+vim.keymap.set("n", "<leader>tb", tbi.buffers, { desc = "view buffers" })
+vim.keymap.set("n", "<leader>th", tbi.help_tags, { desc = "view help tags" })
+vim.keymap.set("n", "<leader>tc", tbi.commands, { desc = "view all commands" })
+vim.keymap.set("n", "<leader>tk", tbi.keymaps, { desc = "view all keymaps" })
+-- " Telescope builtin - tt - telescope telescope
+vim.keymap.set("n", "<leader>twd", tbi.diagnostics, { desc = "entire workspace diagnostics" })
+vim.keymap.set("n", "<leader>tt", tbi.builtin, { desc = "telescope builtin" })
+
+vim.keymap.set("n", "<leader>td", function()
+  tbi.diagnostics { bufnr = 0}
+end, { desc = "current buffer diagnostics" })
+
+--"-H is hidden, -I is ignore, -L is follow, -E is exclude glob
+vim.keymap.set("n", "<leader>taf", function()
+  tbi.find_files { find_command = {"fd", "-H", "-L", "-E", ".git" }}
+end, {desc = "find files custom command no ignore"})
+
+vim.keymap.set("n", "<leader>tif", function()
+  tbi.find_files { find_command = {"fd", "-H", "-I", "-L", "-E", ".git" }}
+end, {desc = "find files custom command do ignore"})
+--" Once you find something <c-x> for open in a horizontal split, <c-v> to open in a vertical split.
+
