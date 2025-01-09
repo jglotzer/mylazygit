6c264e875ba28b36b217fa03a88f3054b38a4460
John Glotzer <jglotzer@gmail.com>
2025-01-08 18:13:20 -0600

    Add user-functions.lua to config.

---
+++ lua/config/user-functions.lua
@@ -0,0 +1,18 @@
+
+vim.api.nvim_create_user_command(
+  "StripTrailingWS",
+  function()
+    local cursor_pos = vim.fn.getpos(".")
+    vim.cmd([[%s/\s\+$//e]])
+    vim.fn.setpos(".", cursor_pos)
+  end,
+  { bang = true, desc = "Strip Trailing Whitespace" }
+)
+
+vim.api.nvim_create_user_command(
+  "BegoneEmptyLines",
+  function()
+    vim.cmd([[g/^$/de]])
+  end,
+  { bang = true, desc = "Begone Empty Lines" }
+)
