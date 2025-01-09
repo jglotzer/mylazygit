bb9bcd398bf6d402dc265ff98dafabfb79763709
John Glotzer <jglotzer@gmail.com>
2025-01-09 17:12:57 -0600

    Add quite a few nice keymaps.

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,4 +1,3 @@
-
 -- Work with panes
 vim.keymap.set("n", "<F9>", "<C-W><C-R>", { desc = "Switch Diff panes around", noremap = true, silent = true })
 vim.keymap.set("n", "<F12>", "<C-W><C-W>", { desc = "Go to other Window", noremap = true, silent = true })
@@ -7,8 +6,27 @@ vim.keymap.set("n", "<F12>", "<C-W><C-W>", { desc = "Go to other Window", norema
 vim.keymap.set("n", "<F11>", "<Cmd>set nu! rnu! <CR>", { desc = "Toggle line numbers", noremap = true, silent = true })
 
 -- In visual mode move selected blocks up and down
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", {})
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", {})
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move block down", noremap = true, silent = true })
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move block up", noremap = true, silent = true })
 
 -- Terminal mode escape.
-vim.keymap.set("t", "<esc><esc>", "<c-\\><c-n>")
+vim.keymap.set("t", "<esc><esc>", "<c-\\><c-n>", { desc = "Terminal mode escape", noremap = true, silent = true })
+
+-- Quickly insert an empty new line without entering insert mode
+vim.keymap.set("n", "<leader>o", "o<esc>",
+  { desc = "Empty newline below, stay in normal mode", noremap = true, silent = true })
+vim.keymap.set("n", "<leader>O", "O<esc>",
+  { desc = "Empty newline above, stay in normal mode", noremap = true, silent = true })
+
+-- TJ - SpaceX SpaceSpaceX source code
+vim.keymap.set("n", "<space><space>x", "<cmd>.lua<CR>", { desc = "Source the current line", noremap = true })
+vim.keymap.set("n", "<space>x", "<cmd>source %<CR>", { desc = "Source the current file", noremap = true })
+--print("Thanks TJ!")
+--These mappings control the size of the splits
+vim.keymap.set("n", "<M-Left>", "<c-w>5>", { desc = "Alt left arrow to move split left", noremap = true })
+vim.keymap.set("n", "<M-Right>", "<c-w>5<", { desc = "Alt right arrow to move split right", noremap = true })
+vim.keymap.set("n", "<M-Up>", "<c-W>5+>", { desc = "Alt up arrow to move split up", noremap = true })
+vim.keymap.set("n", "<M-Down>", "<c-W>5-<", { desc = "Alt down arrow to move split down", noremap = true })
+
+vim.keymap.set("n", "<leader><CR>", "<cmd>source /home/jglotzer/.lazy/nvim/init.lua <CR>",
+  { desc = "Source init.lua (shoutout)", noremap = true })
