910add145b47d4928363e527123d73cf04784b45
John Glotzer <jglotzer@gmail.com>
2025-01-09 21:11:56 -0600

    redundant repo name for nvim-lspconfig

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -23,7 +23,6 @@ return {
   'hrsh7th/nvim-cmp',
   'hrsh7th/cmp-buffer',
   'hrsh7th/cmp-path',
-  'neovim/nvim-lspconfig',
   'hrsh7th/cmp-nvim-lua',
   'hrsh7th/cmp-nvim-lsp',
   'hrsh7th/cmp-cmdline',
