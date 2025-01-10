df55a676e09d4dc7cbf5e7be064e48af1cec01d9
John Glotzer <jglotzer@gmail.com>
2025-01-10 15:26:59 -0600

    For fzf don't think that ESC hide alias is needed anymore.

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -2,17 +2,5 @@ return {
   "ibhagwan/fzf-lua",
   dependencies = { "nvim-tree/nvim-web-devicons" },
   opts = {},
-  -- Seems like a bug. Seems like if you go to a file from fzf-lua you can't run the files command
-  -- again, instead you have to resume and resume doesn't always work. By hitting escape you ensure
-  -- that resume *will* always work.
-  -- By default pressing esc or ctrl-c terminates the fzf process, as such resume is not perfect and
-  -- is limited to resuming the picker/query and sometimes additional parameters such as regex in
-  -- grep, etc, for a more "complete" resume press alt-esc to hide the fzf process instead, this
-  -- will keep the fzf process running in the background and thus will restore the process entirely
-  -- including cursor position and selection. To configure hiding by default:
-  -- Actually the real difficulty was a naming conflict on <leader>F which happened on LSP attach.
-  -- Now that that conflict has been fixed there is no issue.
-  config = require('fzf-lua').setup(
-    { 'fzf-native' },
-    { keymap = { builtin = { true, ["<Esc>"] = "hide" } } })
+  config = require('fzf-lua').setup({ 'fzf-native' })
 }
