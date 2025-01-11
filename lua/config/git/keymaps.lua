60042a57848f921016d32aa7082bde25f063e1dd
John Glotzer <jglotzer@gmail.com>
2025-01-11 14:05:35 -0600

    Keymaps for grabbing diffs in merge view.

--- lua/config/git/keymaps.lua
+++ lua/config/git/keymaps.lua
@@ -9,3 +9,17 @@ vim.keymap.set('n', '<leader>gm', ':Git mergetool<CR>')
 -- Improve diff experience
 vim.opt.diffopt:append('algorithm:patience')
 vim.opt.diffopt:append('indent-heuristic')
+
+-- Quick conflict resolution
+vim.keymap.set('n', '<leader>gf', ':diffget 1<CR>')  -- get from left
+vim.keymap.set('n', '<leader>gj', ':diffget 2<CR>')  -- get from right
+
+-- Show conflict stats
+vim.cmd([[
+function! ConflictStats()
+    let l:conflict_pattern = '^<<<<<<< '
+    let l:conflicts = search(l:conflict_pattern, 'n')
+    echo "Remaining conflicts: " . l:conflicts
+endfunction
+]])
+vim.keymap.set('n', '<leader>gc', ':call ConflictStats()<CR>')
