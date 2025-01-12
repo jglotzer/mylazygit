213c70f8e549ff435dc8236f801040f86d51a7ff
John Glotzer <jglotzer@gmail.com>
2025-01-11 20:17:49 -0600

    improved git keymaps

--- lua/config/git/keymaps.lua
+++ lua/config/git/keymaps.lua
@@ -1,21 +1,22 @@
 -- https://www.linkedin.com/pulse/mastering-three-way-merge-neovim-fugitive-delta-joey-wang-ldtfc/
--- Helpful keymaps for Git operations
-vim.keymap.set('n', '<leader>gs', ':Git<CR>')
-vim.keymap.set('n', '<leader>gd', ':Gdiffsplit<CR>')
-vim.keymap.set('n', '<leader>gc', ':Git commit<CR>')
-vim.keymap.set('n', '<leader>gb', ':Git blame<CR>')
-vim.keymap.set('n', '<leader>gm', ':Git mergetool<CR>')
+-- Keymaps for Git operations
+vim.keymap.set('n', '<leader>gs', ':Git<CR>', { desc = 'Git status' })
+vim.keymap.set('n', '<leader>gd', ':Gvdiffsplit<CR>', { desc = 'Git vdiff split' })
+vim.keymap.set('n', '<leader>gc', ':Git commit<CR>', { desc = 'Git Commit' })
+vim.keymap.set('n', '<leader>gb', ':Git blame<CR>', { desc = 'Git Blame' })
+vim.keymap.set('n', '<leader>gm', ':Git mergetool<CR>', { desc = 'Git Merge' })
 
 -- Improve diff experience
 vim.opt.diffopt:append('algorithm:patience')
 vim.opt.diffopt:append('indent-heuristic')
 
--- Quick conflict resolution
-vim.keymap.set('n', '<leader>dg1', ':diffget 1<CR>')  -- get from left
-vim.keymap.set('n', '<leader>dg2', ':diffget 2<CR>')  -- get from right
-vim.keymap.set('n', '<leader>dp',  ':diffput 3<CR>')  -- put from this buffer
+-- Conflict resolution
+vim.keymap.set('n', '<leader>m1', ':diffget 1<CR>', { desc = 'merge from left buffer' })
+vim.keymap.set('n', '<leader>m2', ':diffget 2<CR>', { desc = 'merge from right buffer' })
+vim.keymap.set('n', '<leader>mp', ':diffput 3<CR>', { desc = 'merge from this buffer' })
 
 -- Show conflict stats
+-- Not super interesting
 vim.cmd([[
 function! ConflictStats()
     let l:conflict_pattern = '^<<<<<<< '
@@ -23,4 +24,4 @@ function! ConflictStats()
     echo "Remaining conflicts: " . l:conflicts
 endfunction
 ]])
-vim.keymap.set('n', '<leader>gc', ':call ConflictStats()<CR>')
+vim.keymap.set('n', '<leader>zz', ':call ConflictStats()<CR>')
