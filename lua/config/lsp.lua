d9ad262e4212360a322b0b635c1f8fa5704f145a
John Glotzer <jglotzer@gmail.com>
2025-01-14 20:07:48 -0600

    nit

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -463,7 +463,6 @@ lspconfig.lua_ls.setup(config({
 }))
 
 local snippets_paths = function()
-  -- local paths = {}
   local friendly_snippets = '/home/jglotzer/.local/share/nvim/lazy/friendly-snippets'
   local my_snippets = "/home/jglotzer/.config/nvim/my-snippets"
   local paths = { friendly_snippets, my_snippets }
