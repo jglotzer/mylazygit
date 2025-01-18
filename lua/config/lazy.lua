088e2d18d2b932ff8a27b01747f52d9b072dacfe
John Glotzer <jglotzer@gmail.com>
2025-01-17 19:21:44 -0600

    Only check for updates every 24h

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -30,7 +30,7 @@ require("lazy").setup({
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
   install = { colorscheme = { "habamax" } },
-  -- automatically check for plugin updates
-  checker = { enabled = true },
+  -- automatically check for plugin updates (every 24 hours)
+  checker = { enabled = true, frequency = 86400 },
 })
 require("config.sets")
