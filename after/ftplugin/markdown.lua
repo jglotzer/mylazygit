de1d4462a3b1b90cede61477d2fec8e95b1dda0d
John Glotzer <jglotzer@gmail.com>
2025-01-27 08:46:11 -0600

    More spell stuff

---
+++ after/ftplugin/markdown.lua
@@ -0,0 +1,16 @@
+-- Markdown specific settings
+vim.opt.wrap = true -- Wrap text
+vim.opt.breakindent = true -- Match indent on line break
+vim.opt.linebreak = true -- Line break on whole words
+vim.opt.textwidth = 100
+
+-- Allow j/k when navigating wrapped lines
+vim.keymap.set("n", "j", "gj")
+vim.keymap.set("n", "k", "gk")
+
+-- Spell check
+-- <space>ss gives spelling suggestions via FZF
+-- z= also gives spelling suggestions
+-- zg marks word as correct
+vim.opt.spelllang = 'en_us,fr'
+vim.opt.spell = true
