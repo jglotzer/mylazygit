9814076e1269602b093cde8770bb6d5d7bde5509
John Glotzer <jglotzer@gmail.com>
2025-02-10 21:25:10 -0600

    small change to none-ls diagnostics.

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -443,7 +443,8 @@ null_ls.setup({
   sources = {
     null_ls.builtins.formatting.stylua,
     null_ls.builtins.completion.spell,
-    null_ls.builtins.diagnostics.eslint,
+    -- null_ls.builtins.diagnostics.eslint,
+    require("none-ls.diagnostics.eslint"),
     null_ls.builtins.formatting.prettier.with({
       enabled_filetypes = js_filetypes,
     }),
