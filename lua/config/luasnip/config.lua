2294672af8a2abcdf956521c20972aed70758e7e
John Glotzer <jglotzer@gmail.com>
2025-02-11 09:58:16 -0600

    renaming on dead code.

--- lua/config/luasnip/config.lua
+++ lua/config/luasnip/config.lua
@@ -1,5 +1,5 @@
-local ls = require "luasnip"
-local types = require "luasnip.util.types"
+local luasnip = require "luasnip"
+local luasnipTypes = require "luasnip.util.types"
 
 local snippets_paths = function()
   local friendly_snippets = '/home/jglotzer/.local/share/nvim/lazy/friendly-snippets'
@@ -7,7 +7,7 @@ local snippets_paths = function()
   return { friendly_snippets, my_snippets }
 end
 
-ls.config.set_config {
+luasnip.config.set_config {
   -- This tells Luasnip to remember to keep around the last snippet.
   -- You can jump back to into it even if you move outside the selection.
   history = true,
@@ -22,7 +22,7 @@ ls.config.set_config {
   -- #vid3
   -- ext_opts = nil,
   ext_opts = {
-    [types.choiceNode] = {
+    [luasnipTypes.choiceNode] = {
       active = {
         virt_text = { { "⟵", "Error" } },
       },
