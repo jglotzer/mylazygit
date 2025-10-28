4bc8e051bf82417020beb137e2af868e1191b99f
John Glotzer <jglotzer@gmail.com>
2025-10-28 10:47:53 +0100

    Python formatting changes.

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -364,6 +364,25 @@ local lsps = {
       on_attach = on_attach,
     },
   },
+  {
+  "pylsp",
+  {
+      capabilities = capabilities,
+      snippetSupport = true,
+      on_attach = on_attach,
+      settings = {
+        pylsp = {
+          plugins = {
+            pycodestyle = {
+              enabled = true,
+              maxLineLength = 120,
+              -- ignore = { "E305", "E221", "E201", "E202" },  -- 👈 suppress “expected 2 blank lines after class or function”, "multiple spaces"
+            },
+          },
+        },
+      },
+    },
+  },
 }
 
 for _, old_lady_judges_watch_people in pairs(lsps) do
@@ -383,6 +402,9 @@ null_ls.setup({
     require("none-ls.diagnostics.eslint"),
     null_ls.builtins.formatting.prettier.with({
       enabled_filetypes = js_filetypes,
+        -- new Python section
+    null_ls.builtins.formatting.black,
+    null_ls.builtins.diagnostics.ruff,
     }),
   },
 })
